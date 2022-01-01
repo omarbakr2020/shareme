503292b15c279474d7ab4303fb947caef5502acc
Omar Bakr <omarbakr301@gmail.com>
2022-01-01 05:06:10 +0200

    Initial commit

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -1,10 +1,10 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator';
+import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type';
-import pin from './pin';
+import schemaTypes from 'all:part:@sanity/base/schema-type'
 import user from './user';
+import pin from './pin';
 import comment from './comment';
 import postedBy from './postedBy';
 import save from './save';
@@ -15,5 +15,7 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([pin, user, postedBy, comment, save]),
-});
+  types: schemaTypes.concat([
+    user, pin, comment, postedBy, save
+  ]),
+})
